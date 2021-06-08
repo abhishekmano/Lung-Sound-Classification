e0d8cde4aac5777ff299f4d7ae059d30a7c726d3
Nithin M Warrier <47141039+NithinWarrier@users.noreply.github.com>
2021-06-08 18:31:40 +0530

    UI changes (again)

--- upload code/app.py
+++ upload code/app.py
@@ -1,9 +1,12 @@
 import json
 import os
-from loadmodel import loadmodel
+
+# from loadmodel import loadmodel
 
 from flask import Flask, render_template, request
+from flask.wrappers import Response
 from werkzeug.utils import secure_filename
+from werkzeug.wrappers import response
 
 app = Flask(__name__)  # reference this file
 
@@ -18,12 +21,23 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
+        per_val = request.form['per_val']
+        
+        audio_len = request.form['audio_len']
+        
+        
         f = request.files['file']
         filename = secure_filename(f.filename)
-
+        
         # to store the filename to json file (mydata.json)
         name = {}
-        name = {'file_name': filename}
+        name = {
+            'file_name': filename,
+                       
+            'length' : audio_len
+        }
+
 
         with open('mydata.json', 'w') as j:
             json.dump(name, j)
@@ -31,15 +45,14 @@ def upload_file():
         # save the file to directory
         f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
 
-        result = loadmodel(filename)  # run loadmodel.py
+        # result = loadmodel(filename)  # run loadmodel.py
 
-        return render_template('uploaded.html', filename=filename, result=result)
+        return render_template('uploaded.html', filename=filename, per_val=per_val)
 
 
 @app.route('/', methods=["back"])
 def go_back():
-    return render_template('index.html')
-
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
